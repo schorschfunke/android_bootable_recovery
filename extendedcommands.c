8c0dfbcb061a2f2ad66f39047ff1fa2348719c9d
fishears <f1she4rs@gmail.com>
2013-01-15 14:38:18 +0000

    recovery: fix mounting internal sd

--- extendedcommands.c
+++ extendedcommands.c
@@ -489,12 +489,13 @@ int control_usb_storage_set_lun(Volume* vol, bool enable, const char *lun_file) 
 
 int control_usb_storage_for_lun(Volume* vol, bool enable) {
     static const char* lun_files[] = {
-#ifdef BOARD_UMS_LUNFILE
-        BOARD_UMS_LUNFILE,
-#endif
-        "/sys/devices/platform/usb_mass_storage/lun%d/file",
-        "/sys/class/android_usb/android0/f_mass_storage/lun/file",
-        "/sys/class/android_usb/android0/f_mass_storage/lun_ex/file",
+//#ifdef BOARD_UMS_LUNFILE
+//        BOARD_UMS_LUNFILE,
+//#endif
+//        "/sys/devices/platform/usb_mass_storage/lun%d/file",
+//        "/sys/class/android_usb/android0/f_mass_storage/lun/file",
+//        "/sys/class/android_usb/android0/f_mass_storage/lun_ex/file",
+	"/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file",
         NULL
     };
 
