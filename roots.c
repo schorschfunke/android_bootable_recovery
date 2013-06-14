6b2bc284c53a1570cc20f869747fa17923cfde8d
Georg Funke <schorsch.funke@googlemail.com>
2013-06-14 17:28:30 +0200

    Fix recovery filesystem table output to log

--- roots.c
+++ roots.c
@@ -157,14 +157,14 @@ void load_volume_table() {
 
     fclose(fstab);
 
-    printf("recovery filesystem table\n");
-    printf("=========================\n");
+    fprintf(stderr, "recovery filesystem table\n");
+    fprintf(stderr, "=========================\n");
     for (i = 0; i < num_volumes; ++i) {
         Volume* v = &device_volumes[i];
-        printf("  %d %s %s %s %s %lld\n", i, v->mount_point, v->fs_type,
+        fprintf(stderr, "  %d %s %s %s %s %lld\n", i, v->mount_point, v->fs_type,
                v->device, v->device2, v->length);
     }
-    printf("\n");
+    fprintf(stderr,"\n");
 }
 
 Volume* volume_for_path(const char* path) {
