87521dd4574ec14e5b23510a4c075ce9a6d9d926
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-18 21:54:22 +0530

    SELinux: don't suppress the "no file_contexts" message on the UI
    Change-Id: I1456e7e2d2a1f3e143c084e1410dd31c505793ad

--- recovery.c
+++ recovery.c
@@ -890,7 +890,7 @@ main(int argc, char **argv) {
 
     if (!sehandle) {
         fprintf(stderr, "Warning: No file_contexts\n");
-        // ui_print("Warning:  No file_contexts\n");
+        ui_print("Warning:  No file_contexts\n");
     }
 
     LOGI("device_recovery_start()\n");
